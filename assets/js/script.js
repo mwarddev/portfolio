c43cc9fd7f309189bd3d849a603c207f919511b0
mwarddev <martinward1981@googlemail.com>
2022-11-08 23:29:06 +0000

    Create parent carousel for all projects and wire up previous and next buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,19 @@ $(document).ready(function() {
         $(this).addClass('active');
     });
 });
+
+// Project carousel buttons
+
+const carouselContainer = document.getElementById("carousel-container");
+const prevButton = document.getElementById("arrow-prev");
+const nextButton = document.getElementById("arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = window.innerWidth;
+    carouselContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = window.innerWidth;
+    carouselContainer.scrollLeft -= slideWidth;
+});
